ead8f39806c9bebc8bf282366303aee680b3644f
Shahla <147069716+ShahlaAbdul@users.noreply.github.com>
2023-10-26 19:58:51 +0400

    Add files via upload

---
+++ week4-4/script.js
@@ -0,0 +1,135 @@
+// let text= " this is programming language";
+// let netice = "this"
+// function myfunction( text1, index=0){
+//     for (let i = 0; i < text1.length; i++) {
+//        if (text1(text1[i]===this)) {
+//         return true;
+//        }    
+//     }
+//     return false;
+
+// }
+// console.log(myfunction(text,"5"))
+
+// const text="programming"
+// const array1=text.split("")
+// console.log(array1)
+
+
+// function myfunction2(text){
+//     text= text.toString()
+//      array2=[]
+// for (let i = 0; i < text.length; i++) {
+//     array2.push(text[i])
+
+// }
+// return array2
+
+// }
+// console.log(myfunction2(34803233))
+
+
+
+// function myfunction( text2){
+//     text2= text2.toString()
+//    let num_enboyuk=text2[0]
+//     for (let i = 0; i < text2.length; i++) {
+//         if (num_enboyuk < text2[i]) {
+
+//             num_enboyuk=text2[i]
+
+//         } 
+//     }
+//     return num_enboyuk
+
+// }
+// console.log(myfunction(136389))
+
+//   ? task14
+// function myfunction( text2,){
+//     text2= text2.toString()
+//      let num_enkicik=text2[0]
+//     for (let i = 0; i < text2.length; i++) {
+//         if (num_enkicik>text2[i]) {
+//             num_enkicik=text2[i]
+
+//         } 
+//     }
+//     return num_enkicik
+
+// }
+// console.log(myfunction(36389))
+// --------------------task15---------------
+// function myfunction(text3) {
+//     text3 = text3.toString()
+//     const array1 = text3.split("")
+//     console.log(array1);
+//     let cem = 0
+//     let hasil = 0
+//     let ededi_orta = 0
+//     for (let i = 0; i < text3.length; i++) {
+//         cem = cem + parseInt(array1[i])
+//         hasil = hasil * parseInt(array1[i])
+//         ededi_orta = ededi_orta * parseInt(array1[i])
+
+        
+//     }
+//      console.log(cem);
+// }
+// console.log(myfunction(3267812))
+
+
+// ----------------task16-----------------------
+
+
+// function myfunction(num){
+//     bolenler=0;
+//     arr=[]
+//     for (let i = 0; i < num; i++) {
+//         if (num%i==0) {
+//             arr.push(i)
+//         }
+//     }
+//     return arr
+// }
+// console.log(myfunction(8));
+
+
+
+// ------------------------task17------------------
+
+
+// function myfunction(num){
+//     bolenler=0;
+//     let count=0
+
+//     for (let i = 0; i <= num; i++) {
+//         if (num%i==0) {
+//            count++
+//            }
+//     }
+//     return count
+// }
+
+// console.log(myfunction(4));
+
+
+// --------------------------------task18------------
+
+function myfunction(num){
+     bolenler=0;
+    let sum=0
+
+    for (let i = 0; i <= num; i++) {
+        if (num%i==0) {
+           sum=sum+sum[i]
+           }
+    }
+    return sum
+}
+
+console.log(myfunction(4));
+
+
+
+
